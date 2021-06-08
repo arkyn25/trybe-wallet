f8de9c192f9836c4f0721954d5ae67d11c67db80
Arlen Freitas <arlenfreitas1@gmail.com>
2021-06-08 15:05:11 -0300

    login

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case xxxx:
+    return { ...state, xxxx: action.xxxx };
+  default:
+    return state;
+  }
+};
+
+export default user;
