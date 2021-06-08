6ba9dc5cf292e70e6d7e26c1a2b46d931d4ed30f
Arlen Freitas <arlenfreitas1@gmail.com>
2021-06-08 20:28:00 -0300

    requisito 6 fix

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
-import { requestCurrencies, addExpense } from '../actions';
+// import { requestCurrencies, addExpense } from '../actions';
 
 const INITIAL_STATE = {
   value: '',
